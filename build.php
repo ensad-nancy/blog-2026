1edc2c763cbe07276d96a009531cf670d5c8a419
Quentin Astié <hello@quentinastie.space>
2026-09-21 11:41:08 +0200

    modification du build

--- build.php
+++ build.php
@@ -1,17 +1,31 @@
 <?php
 @mkdir(__DIR__ . '/dist', 0777, true);
 
+function reecrire_liens(string $html): string {
+    $html = preg_replace_callback(
+        '/article\.php\?student=([^&"\']+)&(?:amp;)?file=([^&"\']+)(?:&(?:amp;)?date=[^"\']*)?/',
+        function ($m) {
+            $student = rawurldecode($m[1]);
+            $nom = basename(rawurldecode($m[2]), '.md');
+            return "article-$student-$nom.html";
+        },
+        $html
+    );
+    // index.php → index.html
+    return str_replace('href="index.php"', 'href="index.html"', $html);
+}
+
+function rendre(string $fichier, array $get = []): string {
+    $code = '$_GET = ' . var_export($get, true) . '; include ' . var_export($fichier, true) . ';';
+    return shell_exec('php -r ' . escapeshellarg($code));
+}
+
+file_put_contents(__DIR__ . '/dist/index.html', reecrire_liens(rendre('index.php')));
+
 foreach (glob(__DIR__ . '/articles/*/*.md') as $chemin) {
     $student = basename(dirname($chemin));
     $file    = basename($chemin);
 
-    $params = ['student' => $student, 'file' => $file];
-    $code   = '$_GET = ' . var_export($params, true) . '; include "article.php";';
-    $html   = shell_exec('php -r ' . escapeshellarg($code));
-
-    // Le lien « Retour » doit pointer vers la page générée
-    $html = str_replace('href="index.php"', 'href="index.html"', $html);
-
-    $sortie = __DIR__ . "/dist/article-$student-" . basename($file, '.md') . '.html';
-    file_put_contents($sortie, $html);
+    $html = reecrire_liens(rendre('article.php', ['student' => $student, 'file' => $file]));
+    file_put_contents(__DIR__ . "/dist/article-$student-" . basename($file, '.md') . '.html', $html);
 }
